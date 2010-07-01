b732f3a8613ccc49e6b668859907e86885fdfebe
Vincent Olivier <vincent@up4.com>
2010-07-01 15:50:08 -0400

    More unfinished work on solr.

--- src/java/org/jhopify/Product.java
+++ src/java/org/jhopify/Product.java
@@ -10,12 +10,14 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 import org.apache.solr.client.solrj.beans.Field;
+import org.jhopify.solr.SolrFacade;
 
 @XmlRootElement(name="product")
 public class Product {
 	
 	@Field String id;
-	Date publishedAt;
+
+	@Field Date publishedAt;
 
 
 	@Field String handle;
@@ -199,27 +201,240 @@ public class Product {
 	}
 	
 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/**********************************
 	* Solr Multivalued Field Wrappers *
-	***********************************/
-	@Field("metafieldName") public void setMetafieldNames(List<String> names) {}
-	@Field("metafieldName") public List<String> getMetafieldNames() {
+	***********************************
+	* JSolr bogusly expects the       *
+	* annotation to be on setters     *
+	* we made setters that do nothing *
+	**********************************/
+
+	
+	@Field("metafieldKey") public void setMetafieldKeys(List<String> key) {}
+	public List<String> getMetafieldNames() {
 		List<String> output = new ArrayList<String>();
+		for(Metafield metafield : getMetafields()) {
+			String key = metafield.getKey();
+			if(key == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(key);
+			}
+		}
 		return output;
 	}
 
 	@Field("metafieldValue") public void setMetafieldValues(List<String> names) {}
+	public List<String> getMetafieldValues() {
+		List<String> output = new ArrayList<String>();
+		for(Metafield metafield : getMetafields()) {
+			String value = metafield.getKey();
+			if(value == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(value);
+			}
+		}
+		return output;
+	}
 
-	@Field("imageURL") public void setImageURLs(List<String> urls) {}
+	@Field("imageSRC") public void setImageSRCs(List<String> urls) {}
+	public List<String> getImageSRCs() {
+		List<String> output = new ArrayList<String>();
+		for(ProductImage image : getImages()) {
+			String imageSRC = image.getSrc();
+			if(imageSRC == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(imageSRC);
+			}
+		}
+		return output;
+	}
 
 	@Field("variantId") public void setVariantIds(List<String> ids) {}
+	public List<String> getVariantIds() {
+		List<String> output = new ArrayList<String>();
+		for(ProductVariant variant : getVariants()) {
+			String id = variant.getId();
+			if(id == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(id);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantTitle") public void setVariantTitles(List<String> titles) {}
+	public List<String> getVariantTitles() {
+		List<String> output = new ArrayList<String>();
+		for(ProductVariant variant : getVariants()) {
+			String title = variant.getTitle();
+			if(title == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(title);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantPosition") public void setVariantPositions(List<Integer> positions) {}
+	public List<Integer> getVariantPositions() {
+		List<Integer> output = new ArrayList<Integer>();
+		for(ProductVariant variant : getVariants()) {
+			Integer position = variant.getPosition();
+			if(position == null) {
+				output.add(SolrFacade.NULL_INTEGER_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(position);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantPrice") public void setVariantPrices(List<Float> prices) {}
+	public List<Float> getVariantPrices() {
+		List<Float> output = new ArrayList<Float>();
+		for(ProductVariant variant : getVariants()) {
+			Float price = variant.getPrice();
+			if(price == null) {
+				output.add(SolrFacade.NULL_FLOAT_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(price);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantPrice") public void setVariantCompareAtPrices(List<Float> prices) {}
+	public List<Float> getVariantCompareAtPrices() {
+		List<Float> output = new ArrayList<Float>();
+		for(ProductVariant variant : getVariants()) {
+			Float price = variant.getCompareAtPrice();
+			if(price == null) {
+				output.add(SolrFacade.NULL_FLOAT_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(price);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantInventoryQuantity") public void setVariantInventoryQuantities(List<Float> quantities) {}
+	public List<Integer> getVariantInventoryQuantities() {
+		List<Integer> output = new ArrayList<Integer>();
+		for(ProductVariant variant : getVariants()) {
+			Integer quantity = variant.getInventoryQuantity();
+			if(quantity == null) {
+				output.add(SolrFacade.NULL_INTEGER_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(quantity);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantOption1Value") public void setVariantOption1Values(List<String> values) {}
+	public List<String> getVariantOption1Values() {
+		List<String> output = new ArrayList<String>();
+		for(ProductVariant variant : getVariants()) {
+			String optionValue = variant.getOption1();
+			if(optionValue == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(optionValue);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantOption2Value") public void setVariantOption2Values(List<String> values) {}
+	public List<String> getVariantOption2Values() {
+		List<String> output = new ArrayList<String>();
+		for(ProductVariant variant : getVariants()) {
+			String optionValue = variant.getOption2();
+			if(optionValue == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(optionValue);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantOption3Value") public void setVariantOption3Values(List<String> values) {}
+	public List<String> getVariantOption3Values() {
+		List<String> output = new ArrayList<String>();
+		for(ProductVariant variant : getVariants()) {
+			String optionValue = variant.getOption3();
+			if(optionValue == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(optionValue);
+			}
+		}
+		return output;
+	}
+
 	@Field("variantSKU") public void setVariantSKUs(List<String> skus) {}
+	public List<String> getVariantSKUs() {
+		List<String> output = new ArrayList<String>();
+		for(ProductVariant variant : getVariants()) {
+			String sku = variant.getSku();
+			if(sku == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(id);
+			}
+		}
+		return output;
+	}
+
+	@Field("variantFirstMetafieldKey") public void setVariantFirstMetafieldKeys(List<String> keys) {}
+	public List<String> getVariantFirstMetafieldKeys() {
+		List<String> output = new ArrayList<String>();
+		for(ProductVariant variant : getVariants()) {
+			String key = null;
+			for(Metafield metafield : variant.getMetafields()) {
+				key = metafield.getKey();
+			}
+			if(key == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(key);
+			}
+		}
+		return output;
+	}
+
+	@Field("variantFirstMetafieldValue") public void setVariantFirstMetafieldValues(List<String> values) {}
+	public List<String> getVariantFirstMetafieldValues() {
+		List<String> output = new ArrayList<String>();
+		for(ProductVariant variant : getVariants()) {
+			String id = variant.getId();
+			if(id == null) {
+				output.add(SolrFacade.NULL_STRING_MULTIVALUED_FIELD_VALUE);
+			} else {
+				output.add(id);
+			}
+		}
+		return output;
+	}
 }
--- src/java/org/jhopify/util/DataMerger.java
+++ src/java/org/jhopify/util/DataMerger.java
@@ -1,7 +1,33 @@
 package org.jhopify.util;
 
+import java.util.Collection;
+import java.util.Map;
+
+import org.jhopify.Product;
+import org.jhopify.ProductVariant;
+
 public class DataMerger {
-	static public void mergeFromIntoDatabase(boolean productIds, boolean variantIds, boolean variantPositions, boolean productImages) {
-		
+	static public void mergeFromIntoDatabase(boolean productIds, boolean variantIds, boolean variantPositions, boolean productImages, Collection<Product> productsFromAPI, Map<String, Product> productsFromDatabase) {
+
+		for(Product productFromAPI : productsFromAPI) {
+			// Look for product in local database
+			Product productFromDatabase = productsFromDatabase.get(productFromAPI.getHandle());
+			if(productFromDatabase == null) {
+				System.out.println("Couldn't find in database product with handle \"" + productFromAPI.getHandle() + "\"");
+			} else {
+				// Iterate over variants from this product (from API)
+				for(ProductVariant variantFromAPI : productFromAPI.getVariants()) {
+					// Look for variant from database…
+					ProductVariant matchingDatabaseVariant = null;
+					for(ProductVariant databaseVariant : productFromDatabase.getVariants()) {
+						// Check if variants from database and API match
+						if(databaseVariant.getSku() != null && databaseVariant.getSku().equals(variantFromAPI.getSku())) {
+							matchingDatabaseVariant = databaseVariant;
+						}
+					}
+					if(matchingDatabaseVariant == null) System.out.println("Couldn't find in database variant with SKU \"" + variantFromAPI.getSku() + "\"");
+				}
+			}
+		}
 	}
 }
